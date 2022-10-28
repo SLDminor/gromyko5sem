d5661866d33e174671b81e72db5da58caa0ebe73
SLDminor <multiplaygames@mail.ru>
2022-10-28 14:52:00 +0300

    interface 1.0

--- app/src/main/java/com/example/ejednevnik/MainActivity.java
+++ app/src/main/java/com/example/ejednevnik/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.ejednevnik;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void showAddButtonChoose(View v){
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        AlertDialog.Builder builder1 = builder.setTitle("Что вы хотите сделать?")
+                .setCancelable(true)
+                .setPositiveButton("Создать дело", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                })
+
+                .setNegativeButton("Редактировать дело", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+    public void goSpisokVtActivity(View v){
+        Intent intent = new Intent(this, Spisok_Activity.class);
+        startActivity(intent);
+    }
+
+    public void goCalendarActivity(View v){
+        Intent intent = new Intent(this, Calendar_Activity.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/example/ejednevnik/Spisok_Activity.java
@@ -0,0 +1,21 @@
+package com.example.ejednevnik;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Spisok_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spisok);
+    }
+
+    public void goBack(View v){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
